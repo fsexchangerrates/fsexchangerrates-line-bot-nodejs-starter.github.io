9e30ff88976419451a25d3926884b860be72c34c
fsexchangerrates <fs.suppermaster@gmail.com>
2020-04-11 11:31:16 +0700

    write

--- messages.js
+++ messages.js
@@ -1,27 +1,4 @@
-const message1 = {
-    type: 'carousel',
-    contents: [{
-            type: 'bubble',
-            size: 'mega'
-        },
-        {
-            type: 'bubble',
-            size: 'mega'
-        },
-        {
-            type: 'bubble',
-            size: 'mega'
-        },
-        {
-            type: 'bubble',
-            size: 'mega'
-        },
-        {
-            type: 'bubble',
-            size: 'mega'
-        }
-    ]
-};
+const message1 = require('./messagesTemp/message1.json');
 module.exports.message1 = message1;
 
 const data1 = {
